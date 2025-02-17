71620ded3db72656799323cf52ce9bfea0b9e0b1
Fandoy <fandyglitch3@gmail.com>
2025-02-17 09:12:17 +0800

    tst

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,5 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  srcDir: "server",
-  compatibilityDate: "2025-02-16"
+  srcDir: "server/routes/api",
+  compatibilityDate: "2025-02-16",
 });
