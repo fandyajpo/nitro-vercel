ba866b2ad9957096b8c9702781ea4899ff85335c
Fandoy <fandyglitch3@gmail.com>
2025-02-20 22:50:14 +0800

    test

--- server/routes/image/index.ts
+++ server/routes/image/index.ts
@@ -1,22 +1,23 @@
-import { createCanvas } from "canvas";
+// import { createCanvas } from "canvas";
 
 export default defineEventHandler({
   handler: (event) => {
-    const width = 400;
-    const height = 200;
+    // const width = 400;
+    // const height = 200;
 
-    const canvas = createCanvas(width, height);
-    const ctx = canvas.getContext("2d");
+    // const canvas = createCanvas(width, height);
+    // const ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "#222";
-    ctx.fillRect(0, 0, width, height);
+    // ctx.fillStyle = "#222";
+    // ctx.fillRect(0, 0, width, height);
 
-    ctx.fillStyle = "#fff";
-    ctx.font = "20px Arial";
-    ctx.fillText(`Fandy Dummy Image`, 50, 100);
+    // ctx.fillStyle = "#fff";
+    // ctx.font = "20px Arial";
+    // ctx.fillText(`Fandy Dummy Image`, 50, 100);
 
-    setHeader(event, "Content-Type", "image/png");
+    // setHeader(event, "Content-Type", "image/png");
 
-    return send(event, canvas.toBuffer("image/png"));
+    // return send(event, canvas.toBuffer("image/png"));
+    return { message: "Under Dev" };
   },
 });
